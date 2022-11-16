eb536f9120c4ffa60af7f09f6952497ec7c474a2
rohanmaharjan27 <rohanmaharjan123@hotmail.com>
2022-11-16 12:57:23 +0545

    changed initalized file to index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import * as msTeams from '@microsoft/teams-js';
 import { Route, Routes } from 'react-router-dom';
 import Configure from './pages/Configure';
 import Home from './pages/Home';
 
 function App() {
-  msTeams.app.initialize();
-
   return (
     <Routes>
       <Route path='/' element={<Home />} />
